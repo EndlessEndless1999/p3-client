af0e2192df726e2611bf85cb48fad3a575d84f0c
salinathapa <salina.business@hotmail.com>
2023-09-11 20:44:39 +0100

    add temp  bg image

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -3,12 +3,13 @@ import React from 'react'
 const HomePage = () => {
   return (
     <>
+      
       <div>
         <h1>Title here</h1>
       </div>
 
       <div className="nav">
-        <a href="#">Play</a>
+        <a href="/play">Play</a>
         <a href="#">High Scores</a>
         <a href="#">Settings</a>
         <a href="#">Help</a>
