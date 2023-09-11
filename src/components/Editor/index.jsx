85dce7e672565395b75a24e3cd1ba33011ab0e67
EndlessEndless1999 <115580045+EndlessEndless1999@users.noreply.github.com>
2023-09-11 11:36:45 +0100

    refactor editor

--- src/components/Editor/index.jsx
+++ src/components/Editor/index.jsx
@@ -2,29 +2,32 @@ import React from 'react'
 import './index.css'
 
 const Editor = () => {
-    return(
-        <div className='editor'>
-            <div className="editor-wrapper">
-                <div className="editor-body">
-                    <div className="editor-code">
 
-                    </div>
+    const onChange = () => {
+        console.log('Hellos');
+    }
+
+    return (
+    <div className="editor">
+        <div className="editor-wrapper">
+            <div className="editor-body">
+                <div id="editorCode" className="editor-code">
+                    
                 </div>
-                <div className="editor-footer">
-                    <div className="editor-footer-left">
-                        <button className='editor-btn editor-run'>Run {'>'} </button>
-                        <button className='editor-btn editor-reset'>Reset {'>'}</button>
-                    </div>
-                    <div className="editor-footer-right">
-                        <div className="editor-console">
-                            <ul className="editor-console-logs">
-
-                            </ul>
-                        </div>
+            </div>
+            <div className="editor-footer">
+                <div className="editor-footer-left">
+                    <button className="editor-btn editor-run">Run {'>'}</button>
+                    <button className="editor-btn editor-reset">Reset {'>'}</button>
+                </div>
+                <div className="editor-footer-right">
+                    <div className="editor-console">
+                        <ul className="editor-console-logs"></ul>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
     )
 }
 
