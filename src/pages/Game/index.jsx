319c95917a8c253b564c0194db427b37a83778af
salinathapa <salina.business@hotmail.com>
2023-09-12 10:21:28 +0100

    add basic div to Game page

--- src/pages/Game/index.jsx
+++ src/pages/Game/index.jsx
@@ -2,7 +2,29 @@ import React from 'react'
 
 const Game = () => {
   return (
-    <div>Game</div>
+    <>
+      <div>
+        <div>
+          <div>
+            <img src="temp-robot.png" alt="BuggyBot" />
+          </div>
+
+          <div>
+            <img src="temp-alien.png" alt="alien" />
+          </div>
+        </div>
+        
+        <div>
+          <div className="code">
+            launch code
+          </div>
+
+          <div className="log">
+            log
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
