d76527ac05b6dbc6c5512f50b50adff2668d4d79
EndlessEndless1999 <115580045+EndlessEndless1999@users.noreply.github.com>
2023-09-12 11:46:42 +0100

    nearly working

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,22 @@ import axios from 'axios';
 
 function App() {
   const [code, setCode] = useState();
+  const [testCases, setTestCases] = useState([]);
 
   const handleClick = (e) => {
     e.preventDefault();
-    axios.post('http://localhost:3000/code', {code}).then((res) => console.log(res))
+    axios.post('http://localhost:3000/python', {code}).then((data) => {
+    console.log(data);  
+    setTestCases[data.passOrFail]})
+    console.log(testCases);
   }
 
 
   return (
     <>
+    <div>{testCases.map((testCase, i) => {
+      return <div key={i}>{testCase}</div>
+    })}</div>
     <NewEditor code={code} setCode={setCode}/>
     <button className='submit-btn' onClick={handleClick}>Submit</button>
     </>
