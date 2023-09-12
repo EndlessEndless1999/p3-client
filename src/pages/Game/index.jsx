ae457b1e81b7f5980a7d80e80f3a6a96372920cc
salinathapa <salina.business@hotmail.com>
2023-09-12 11:42:59 +0100

    add basic css to game

--- src/pages/Game/index.jsx
+++ src/pages/Game/index.jsx
@@ -3,25 +3,29 @@ import React from 'react'
 const Game = () => {
   return (
     <>
-      <div>
+      <div className="timer">
+        Timer
+      </div>
+      
+      <div className="characters">
         <div>
-          <div>
-            <img src="temp-robot.png" alt="BuggyBot" />
-          </div>
-
-          <div>
-            <img src="temp-alien.png" alt="alien" />
-          </div>
+          <img src="temp-robot.png" alt="BuggyBot" />
         </div>
-        
-        <div>
-          <div className="code">
-            launch code
-          </div>
 
-          <div className="log">
-            log
-          </div>
+        <div>
+          <img src="temp-alien.png" alt="alien" />
+          <img src="temp-alien.png" alt="alien" />
+          <img src="temp-alien.png" alt="alien" />
+        </div>
+      </div>
+        
+      <div className="playerContainer">
+        <div className="code">
+          <button>launch code</button>
+        </div>
+
+        <div className="log">
+          log
         </div>
       </div>
     </>
