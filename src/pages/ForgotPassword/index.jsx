9e68c27a6152cedac5dc7b018b0ade99fc3b7746
salinathapa <salina.business@hotmail.com>
2023-09-13 15:23:21 +0100

    create basic forgot password page

--- src/pages/ForgotPassword/index.jsx
+++ src/pages/ForgotPassword/index.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const ForgotPassword = () => {
-  return (
-    <div>ForgotPassword</div>
-  )
-}
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
 
-export default ForgotPassword
+  const handleResetPassword = () => {
+    setMessage('Password reset instructions sent to your email?');
+  };
+
+  return (
+    <div className="forgot">
+      <h2>Forgot Password</h2>
+      <div className="email">
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <button onClick={handleResetPassword}>Reset Password</button>
+      {message && <p>{message}</p>}
+    </div>
+  );
+};
+
+export default ForgotPassword;
