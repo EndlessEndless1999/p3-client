7798ed68bf9ec35457b062ed9741d8e5476c8612
EndlessEndless1999 <115580045+EndlessEndless1999@users.noreply.github.com>
2023-09-13 09:32:22 +0100

    remove unecessary files

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,1 @@
-export { default as EditorWrapper } from './EditorWrapper'
-export { default as Editor } from './Editor'
-export { default as RunButton } from './RunButton'
-export { default as ResetButton } from './ResetButton'
 export { default as NewEditor } from './NewEditor'
